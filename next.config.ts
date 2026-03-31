13ff9bdd6ba8bafbc6da35cd6a11c5d708fd98f3
David Vogeleer <david@individual11.com>
2026-03-31 16:00:30 -0400

    feat: configure static export and add GitHub Actions workflow for automated deployment to GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // This tells Next.js to dump a static HTML/CSS/JS output folder instead of requiring a Node.js server
+  output: 'export',
+  
+  // UNCOMMENT the following line if you host this inside a repository namespace rather than a custom domain name!
+  // Example: If your site is https://your_name.github.io/row_row/, the basePath must exactly match the repository name.
+  // basePath: '/row_row',
 };
 
 export default nextConfig;
